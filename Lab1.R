f8f95986d6cc251125f3b2244795447b0c4d65f9
Andrea Dame <56303840+adame101@users.noreply.github.com>
2020-08-31 17:20:05 -0400

    Add files via upload

---
+++ Lab1.R
@@ -0,0 +1,37 @@
+# example 1
+x= 3
+y= 15
+ x+y
+18
+ x-y
+-12
+x * y
+45
+x/y
+0.2
+
+# example 2
+x=2
+y=3
+x^5 > y^4
+FALSE
+
+# example 3
+x <-c(22, 62, 148, 43, 129)
+sum(x)
+404
+x [1]/sum(x)
+0.05445545
+x [2]/sum(x)
+0.1534653
+x [3]/sum(x)
+0.3663366
+x [4]/sum(x)
+0.1064356
+x [5]/sum(x)
+0.3193069
+
+# example 4
+nucleotides <- c('A', 'T', 'C', 'G')
+sort(nucleotides)
+"A" "C" "G" "T"
